4caa08a914159ad427547644b98cc2fb7a20e1b0
TMMShared <camasinfiniteresource@gmail.com>
2025-06-05 21:07:13 -0700

    Solution to problem 2

--- src/main/kotlin/frc/team2471/off2025/OI.kt
+++ src/main/kotlin/frc/team2471/off2025/OI.kt
@@ -20,12 +20,39 @@ object OI {
     init {
 
         //TODO: Use left and right triggers to run percent output
+        driverController.rightTrigger(0.1).or(driverController.leftTrigger(0.1)).whileTrue(
+            Commands.run({Elevator.setPercentOut(MathUtil.applyDeadband(
+                driverController.rightTriggerAxis - driverController.leftTriggerAxis, 0.1))})
+                .finallyDo(Runnable { Elevator.setPercentOut(0.0) })
+        )
 
-        //TODO: up Dpad -> move up 1 inch
+        driverController.povUp().onTrue(
+         Commands.runOnce({
+             println("going up ${Elevator.heightInches}")
+             Elevator.setPosition(Elevator.heightInches + 1.0)
+         })
+      )
+      driverController.povDown().onTrue(
+          Commands.runOnce({
+              println("going down ${Elevator.heightInches}")
+              Elevator.setPosition(Elevator.heightInches - 1.0)
+          })
+      )
         //TODO: down Dpad -> move down 1 inch
 
         //TODO: use motionMagic to move up/down 10 inches smoothly using left/right dPad
-
+        driverController.povRight().onTrue(
+            Commands.runOnce({
+                println("going up ${Elevator.heightInches}")
+                Elevator.setMotionMagic(Elevator.heightInches + 10.0)
+            })
+        )
+        driverController.povLeft().onTrue(
+            Commands.runOnce({
+                println("going down ${Elevator.heightInches}")
+                Elevator.setMotionMagic(Elevator.heightInches - 10.0)
+            })
+        )
 
 
 
--- src/main/kotlin/frc/team2471/off2025/subsystems/Elevator.kt
+++ src/main/kotlin/frc/team2471/off2025/subsystems/Elevator.kt
@@ -10,18 +10,34 @@ import com.ctre.phoenix6.signals.GravityTypeValue
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
 import edu.wpi.first.math.MathUtil
+import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.units.measure.Current
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.team2471.frc2025.CANivores
 import org.team2471.frc2025.Falcons
 
 object Elevator: SubsystemBase() {
+
+    private val table = NetworkTableInstance.getDefault().getTable("Armavator")
+    private val elevatorCurrentEntry = table.getEntry("Elevator Current")
+    private val elevatorHeightEntry = table.getEntry("Elevator Motor Height")
+    private val elevatorSetpointEntry = table.getEntry("Elevator Setpoint")
+    private val elevatorVelocityEntry = table.getEntry("Elevator Velocity")
+
     //TODO: Create elevator motor
-
-
+    val motor0 = TalonFX(Falcons.ELEVATOR_0, CANivores.ELEVATOR_CAN)
+    val motor1 = TalonFX(Falcons.ELEVATOR_1, CANivores.ELEVATOR_CAN)
+    val motor2 = TalonFX(Falcons.ELEVATOR_2, CANivores.ELEVATOR_CAN)
+    val motor3 = TalonFX(Falcons.ELEVATOR_3, CANivores.ELEVATOR_CAN)
     val revolutionsPerInch = 1.6
 
 
     //TODO: Get height of elevator from motor rotations using a variable getter and the revolutions per inch ratio
+
+
+    val heightInches: Double
+        get() = motor0.position.valueAsDouble / revolutionsPerInch
+    var heightSetpoint: Double = 0.0
 
 
     val MIN_HEIGHT_INCHES = 0.0
@@ -29,29 +45,62 @@ object Elevator: SubsystemBase() {
 
     init {
         val motorConfigs = TalonFXConfiguration().apply {
-            //TODO: Set safe current limits
-            //TODO: Invert leader and/or followers correctly
-            //TODO: Set kP and kG values
-            //TODO: Configure MotionMagic
+            CurrentLimits.apply {
+                SupplyCurrentLimit = 30.0
+                SupplyCurrentLimitEnable = true
+            }
+        MotorOutput.apply {
+            Inverted = InvertedValue.Clockwise_Positive
+            NeutralMode = NeutralModeValue.Coast
+        }
+            Slot0.apply {
+                kP = 0.5
+                kG = 0.06
+                GravityType = GravityTypeValue.Elevator_Static
+            }
+            MotionMagic.apply {
+                MotionMagicAcceleration = 25.0 * revolutionsPerInch
+                MotionMagicCruiseVelocity = 35.0 * revolutionsPerInch
+            }
         }
 
-        //TODO: apply configs to all motors
+        motor0.configurator.apply(motorConfigs)
+        motor1.configurator.apply(motorConfigs)
+        motor2.configurator.apply(motorConfigs)
+        motor3.configurator.apply(motorConfigs)
 
-        //TODO: Tell motors to follow leader
+
+        motor1.setControl(Follower(motor0.deviceID, true))
+        motor2.setControl(Follower(motor0.deviceID, false))
+        motor3.setControl(Follower(motor0.deviceID, false))
     }
 
+    override fun periodic()
+    {
+        // This method will be called once per scheduler run
+        elevatorHeightEntry.setDouble(heightInches)
+        elevatorSetpointEntry.setDouble(heightSetpoint)
+        elevatorCurrentEntry.setDouble(motor0.statorCurrent.valueAsDouble)
+        elevatorVelocityEntry.setDouble(motor0.velocity.valueAsDouble)
+    }
 
     fun setPercentOut(percent: Double) {
-        //TODO: Set percent output of motors
+        motor0.setControl(DutyCycleOut(percent))
+        println("percentage: $percent")
     }
 
     fun setPosition(inches: Double) {
-        //TODO: Set position of motors
+        motor0.setControl(PositionDutyCycle(MathUtil.clamp(inches, MIN_HEIGHT_INCHES, MAX_HEIGHT_INCHES) * revolutionsPerInch))
+        println("position: $inches")
+        heightSetpoint = inches
     }
 
 
     //TODO: Make a set motionMagic function that sets position using motionMagic
-
-
+    fun setMotionMagic(inches: Double) {
+        motor0.setControl(PositionDutyCycle(MathUtil.clamp(inches, MIN_HEIGHT_INCHES, MAX_HEIGHT_INCHES) * revolutionsPerInch))
+        println("position: $inches")
+        heightSetpoint = inches
+    }
 
 }
