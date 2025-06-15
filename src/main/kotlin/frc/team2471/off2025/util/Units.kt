6e66f4681e9591617ce2e84226fdc16d12ac8d51
Justin Kilk <justin@kilk.com>
2025-06-15 11:42:24 -0700

    added .perSecond onto Distance and Velocity units. Extends the Double class everywhere instead of Number.

--- src/main/kotlin/frc/team2471/off2025/util/Units.kt
+++ src/main/kotlin/frc/team2471/off2025/util/Units.kt
@@ -1,3 +1,4 @@
+@file:Suppress("UNUSED")
 package frc.team2471.off2025.util
 
 import edu.wpi.first.math.geometry.Rotation2d
@@ -17,11 +18,11 @@ inline val Distance.asMeters: Double get() = `in`(Meters)
 inline val Distance.asCentimeters: Double get() = `in`(Centimeters)
 inline val Distance.asMillimeters: Double get() = `in`(Millimeter)
 
-inline val Number.inches: Distance get() = Inches.of(this.toDouble())
-inline val Number.feet: Distance get() = Feet.of(this.toDouble())
-inline val Number.meters: Distance get() = Meters.of(this.toDouble())
-inline val Number.centimeters: Distance get() = Centimeters.of(this.toDouble())
-inline val Number.millimeters: Distance get() = Millimeters.of(this.toDouble())
+inline val Double.inches: Distance get() = Inches.of(this)
+inline val Double.feet: Distance get() = Feet.of(this)
+inline val Double.meters: Distance get() = Meters.of(this)
+inline val Double.centimeters: Distance get() = Centimeters.of(this)
+inline val Double.millimeters: Distance get() = Millimeters.of(this)
 
 
 //Angle
@@ -29,9 +30,9 @@ inline val Angle.asDegrees: Double get() = `in`(Degrees)
 inline val Angle.asRotations: Double get() = `in`(Rotations)
 inline val Angle.asRadians: Double get() = `in`(Radians)
 
-inline val Number.degrees: Angle get() = Degrees.of(this.toDouble())
-inline val Number.rotations: Angle get() = Rotations.of(this.toDouble())
-inline val Number.radians: Angle get() = Radians.of(this.toDouble())
+inline val Double.degrees: Angle get() = Degrees.of(this)
+inline val Double.rotations: Angle get() = Rotations.of(this)
+inline val Double.radians: Angle get() = Radians.of(this)
 
 
 //Time
@@ -40,10 +41,10 @@ inline val Time.asMinutes: Double get() = `in`(Minutes)
 inline val Time.asMilliseconds: Double get() = `in`(Milliseconds)
 inline val Time.asMicroseconds: Double get() = `in`(Microseconds)
 
-inline val Number.seconds: Time get() = Seconds.of(this.toDouble())
-inline val Number.milliseconds: Time get() = Milliseconds.of(this.toDouble())
-inline val Number.microseconds: Time get() = Microseconds.of(this.toDouble())
-inline val Number.minutes: Time get() = Minutes.of(this.toDouble())
+inline val Double.seconds: Time get() = Seconds.of(this)
+inline val Double.milliseconds: Time get() = Milliseconds.of(this)
+inline val Double.microseconds: Time get() = Microseconds.of(this)
+inline val Double.minutes: Time get() = Minutes.of(this)
 
 
 //Linear Velocity
@@ -51,9 +52,11 @@ inline val LinearVelocity.asInchesPerSecond: Double get() = `in`(InchesPerSecond
 inline val LinearVelocity.asFeetPerSecond: Double get() = `in`(FeetPerSecond)
 inline val LinearVelocity.asMetersPerSecond: Double get() = `in`(MetersPerSecond)
 
-inline val Number.asInchesPerSecond: LinearVelocity get() = InchesPerSecond.of(this.toDouble())
-inline val Number.feetPerSecond: LinearVelocity get() = FeetPerSecond.of(this.toDouble())
-inline val Number.metersPerSecond: LinearVelocity get() = MetersPerSecond.of(this.toDouble())
+inline val Distance.perSecond: LinearVelocity get() = InchesPerSecond.of(this.asInches)
+
+inline val Double.inchesPerSecond: LinearVelocity get() = InchesPerSecond.of(this)
+inline val Double.feetPerSecond: LinearVelocity get() = FeetPerSecond.of(this)
+inline val Double.metersPerSecond: LinearVelocity get() = MetersPerSecond.of(this)
 
 
 //Angular Velocity
@@ -62,10 +65,12 @@ inline val AngularVelocity.asRotationsPerSecond: Double get() = `in`(RotationsPe
 inline val AngularVelocity.asRPM: Double get() = `in`(RPM)
 inline val AngularVelocity.asRadiansPerSecond: Double get() = `in`(RadiansPerSecond)
 
-inline val Number.degreesPerSecond: AngularVelocity get() = DegreesPerSecond.of(this.toDouble())
-inline val Number.rotationsPerSecond: AngularVelocity get() = RotationsPerSecond.of(this.toDouble())
-inline val Number.rpm: AngularVelocity get() = RPM.of(this.toDouble())
-inline val Number.radiansPerSecond: AngularVelocity get() = RadiansPerSecond.of(this.toDouble())
+inline val Angle.perSecond: AngularVelocity get() = DegreesPerSecond.of(this.asDegrees)
+
+inline val Double.degreesPerSecond: AngularVelocity get() = DegreesPerSecond.of(this)
+inline val Double.rotationsPerSecond: AngularVelocity get() = RotationsPerSecond.of(this)
+inline val Double.rpm: AngularVelocity get() = RPM.of(this)
+inline val Double.radiansPerSecond: AngularVelocity get() = RadiansPerSecond.of(this)
 
 
 //Linear Acceleration
@@ -73,10 +78,13 @@ inline val LinearAcceleration.asFeetPerSecondPerSecond: Double get() = `in`(Feet
 inline val LinearAcceleration.asMetersPerSecondPerSecond: Double get() = `in`(MetersPerSecondPerSecond)
 inline val LinearAcceleration.asGs: Double get() = `in`(Gs)
 
-inline val Number.asInchesPerSecondPerSecond: LinearAcceleration get() = InchesPerSecond.per(Second).of(this.toDouble())
-inline val Number.feetPerSecondPerSecond: LinearAcceleration get() = FeetPerSecondPerSecond.of(this.toDouble())
-inline val Number.metersPerSecondPerSecond: LinearAcceleration get() = MetersPerSecondPerSecond.of(this.toDouble())
-inline val Number.Gs: LinearAcceleration get() = Units.Gs.of(this.toDouble())
+inline val Distance.perSecondPerSecond: LinearAcceleration get() = FeetPerSecondPerSecond.of(this.asFeet)
+inline val LinearVelocity.perSecond: LinearAcceleration get() = FeetPerSecondPerSecond.of(this.asFeetPerSecond)
+
+inline val Double.asInchesPerSecondPerSecond: LinearAcceleration get() = InchesPerSecond.per(Second).of(this)
+inline val Double.feetPerSecondPerSecond: LinearAcceleration get() = FeetPerSecondPerSecond.of(this)
+inline val Double.metersPerSecondPerSecond: LinearAcceleration get() = MetersPerSecondPerSecond.of(this)
+inline val Double.Gs: LinearAcceleration get() = Units.Gs.of(this)
 
 
 //Angular Acceleration
@@ -84,9 +92,12 @@ inline val AngularAcceleration.asDegreesPerSecondPerSecond: Double get() = `in`(
 inline val AngularAcceleration.asRotationsPerSecondPerSecond: Double get() = `in`(RotationsPerSecondPerSecond)
 inline val AngularAcceleration.asRadiansPerSecondPerSecond: Double get() = `in`(RadiansPerSecondPerSecond)
 
-inline val Number.degreesPerSecondPerSecond: AngularAcceleration get() = DegreesPerSecondPerSecond.of(this.toDouble())
-inline val Number.rotationsPerSecondPerSecond: AngularAcceleration get() = RotationsPerSecondPerSecond.of(this.toDouble())
-inline val Number.radiansPerSecondPerSecond: AngularAcceleration get() = RadiansPerSecondPerSecond.of(this.toDouble())
+inline val Angle.perSecondPerSecond: AngularAcceleration get() = DegreesPerSecondPerSecond.of(this.asDegrees)
+inline val AngularVelocity.perSecond: AngularAcceleration get() = DegreesPerSecondPerSecond.of(this.asDegreesPerSecond)
+
+inline val Double.degreesPerSecondPerSecond: AngularAcceleration get() = DegreesPerSecondPerSecond.of(this)
+inline val Double.rotationsPerSecondPerSecond: AngularAcceleration get() = RotationsPerSecondPerSecond.of(this)
+inline val Double.radiansPerSecondPerSecond: AngularAcceleration get() = RadiansPerSecondPerSecond.of(this)
 
 
 //Mass
@@ -95,10 +106,10 @@ inline val Mass.asGrams: Double get() = `in`(Grams)
 inline val Mass.asPounds: Double get() = `in`(Pounds)
 inline val Mass.asOunces: Double get() = `in`(Ounces)
 
-inline val Number.kilograms: Mass get() = Kilograms.of(this.toDouble())
-inline val Number.grams: Mass get() = Grams.of(this.toDouble())
-inline val Number.pounds: Mass get() = Pounds.of(this.toDouble())
-inline val Number.ounces: Mass get() = Ounces.of(this.toDouble())
+inline val Double.kilograms: Mass get() = Kilograms.of(this)
+inline val Double.grams: Mass get() = Grams.of(this)
+inline val Double.pounds: Mass get() = Pounds.of(this)
+inline val Double.ounces: Mass get() = Ounces.of(this)
 
 
 //The Force
@@ -106,9 +117,9 @@ inline val Force.asNewtons: Double get() = `in`(Newtons)
 inline val Force.asOuncesForce: Double get() = `in`(OuncesForce)
 inline val Force.asPoundsForce: Double get() = `in`(PoundsForce)
 
-inline val Number.newtons: Force get() = Newtons.of(this.toDouble())
-inline val Number.ouncesForce: Force get() = OuncesForce.of(this.toDouble())
-inline val Number.poundsForce: Force get() = PoundsForce.of(this.toDouble())
+inline val Double.newtons: Force get() = Newtons.of(this)
+inline val Double.ouncesForce: Force get() = OuncesForce.of(this)
+inline val Double.poundsForce: Force get() = PoundsForce.of(this)
 
 
 //Torque
@@ -116,19 +127,19 @@ inline val Torque.asNewtonMeters: Double get() = `in`(NewtonMeters)
 inline val Torque.asPoundFeet: Double get() = `in`(PoundFeet)
 inline val Torque.asOunceInches: Double get() = `in`(OunceInches)
 
-inline val Number.newtonMeters: Torque get() = NewtonMeters.of(this.toDouble())
-inline val Number.poundFeet: Torque get() = PoundFeet.of(this.toDouble())
-inline val Number.ounceInches: Torque get() = OunceInches.of(this.toDouble())
+inline val Double.newtonMeters: Torque get() = NewtonMeters.of(this)
+inline val Double.poundFeet: Torque get() = PoundFeet.of(this)
+inline val Double.ounceInches: Torque get() = OunceInches.of(this)
 
 
 //MOI
-inline val Number.kilogramSquareMeters: MomentOfInertia get() = KilogramSquareMeters.of(this.toDouble())
+inline val Double.kilogramSquareMeters: MomentOfInertia get() = KilogramSquareMeters.of(this)
 
 inline val MomentOfInertia.asKilogramSquareMeters: Double get() = `in`(KilogramSquareMeters)
 
 
 //Voltage
-inline val Number.volts: Voltage get() = Volts.of(this.toDouble())
+inline val Double.volts: Voltage get() = Volts.of(this)
 
 inline val Voltage.asVolts: Double get() = `in`(Volts)
 
@@ -136,11 +147,11 @@ inline val Voltage.asVolts: Double get() = `in`(Volts)
 //Current
 inline val Current.asAmps: Double get() = `in`(Amps)
 
-inline val Number.amps: Current get() = Amps.of(this.toDouble())
+inline val Double.amps: Current get() = Amps.of(this)
 
 
 //Other
-inline val Number.voltsPerSecond: Velocity<VoltageUnit> get() = Volts.per(Second).of(this.toDouble())
+inline val Double.voltsPerSecond: Velocity<VoltageUnit> get() = Volts.per(Second).of(this)
 
 inline val Velocity<VoltageUnit>.asVoltsPerSecond: Double get() = `in`(Volts.per(Second))
 
@@ -150,8 +161,8 @@ fun LinearVelocity.toAngular(radius: Distance) = RadiansPerSecond.of(this.asMete
 fun AngularVelocity.toLinear(radius: Distance) = MetersPerSecond.of(this.asRadiansPerSecond * radius.asMeters)!!
 
 
-/**Converts a [Number] in hertz into an equivalent [Time] unit.*/
-fun Number.hertzToTime() = if (this == 0.0) 0.seconds else (1.0 / this.toDouble()).seconds
+/**Converts a [Double] in hertz into an equivalent [Time] unit.*/
+fun Double.hertzToTime() = if (this == 0.0) 0.0.seconds else (1.0 / this).seconds
 
 @JvmName("sinOf")
 fun sin(angle: Angle) = sin(angle.asRadians)
