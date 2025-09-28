53d98a42a34e9e457907b63c0828743eaaf3671b
Justin Kilk <justin@kilk.com>
2025-09-28 14:37:51 -0700

    added comments and todo to climb subsystem

--- src/main/kotlin/frc/team2471/off2025/Climb.kt
+++ src/main/kotlin/frc/team2471/off2025/Climb.kt
@@ -1,45 +1,39 @@
 package frc.team2471.off2025
 
-import com.ctre.phoenix6.controls.DutyCycleOut
 import com.ctre.phoenix6.hardware.TalonFX
 import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.team2471.off2025.util.units.asDegrees
+import frc.team2471.off2025.util.units.rotations
 import org.littletonrobotics.junction.Logger
 
 object Climb: SubsystemBase("Climb") {
     val climberMotor = TalonFX(Falcons.CLIMBER_MOTOR)
     val switch = DigitalInput(DigitalSensors.CLIMBER)
 
-    var climbPowerSetpoint: Double = 0.0
-        set(value) {
-            if (relayOn) {
-                field = 0.0 // Relay is on, stop moving
-            } else {
-                field = value.coerceIn(0.0, 1.0) // Motor should never go backwards.
-            }
-            climberMotor.setControl(DutyCycleOut(field))
-        }
 
     val relayOn: Boolean get() = !switch.get()
 
 
     init {
         println("Climb init")
-        // configure climber motor.
+
+        //TODO: configure climber motor.
         // 20A continuous limit, 30A peak limit, 1 second peak duration
         // coast mode, no invert
         // gear ratio is 45:1
     }
 
 
+    // Logging periodic. TODO: Set motor power to 0.0 if the relay is active.
     override fun periodic() {
-        if (relayOn) {
-            climbPowerSetpoint = 0.0
-        }
         Logger.recordOutput("Climb/relayOn", relayOn)
-        Logger.recordOutput("Climb/powerSetpoint", climbPowerSetpoint)
+        Logger.recordOutput("Climb/motorPower", climberMotor.dutyCycle.valueAsDouble)
+        Logger.recordOutput("Climb/motorPositionDeg", climberMotor.position.valueAsDouble.rotations.asDegrees)
     }
 
 
-    // needs function that can "deploy" the climber by doing a 180 degree turn (or more) forward. A PID or bang-bang controller should work
+    //TODO: needs function that can "deploy" the climber by doing a 180 degree turn (or more) forward. A PID or bang-bang controller should work
+
+    //TODO: needs function to "climb!!!!." Retracts the climber at full power. Should stop when relay is triggered.
 }
